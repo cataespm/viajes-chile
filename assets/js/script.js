ba25d450f29e0ede755dd804065acb042c6c64d4
cata <kat.espinosa.m@gmail.com>
2024-07-01 19:28:16 -0400

    integramos js y scrollbar

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+$("#enviar").click(function () {
+  alert("El correo fue enviado");
+});
